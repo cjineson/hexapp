48a55d095e7ef56dfa843ca2a09f4fd4cf3b769a
Chris Ineson <chrisineson@Chriss-MacBook-Pro.local>
2019-07-10 11:06:01 +0100

    Initial customise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './hex.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={hex.png} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
           Team Hex
       </header>
     </div>
